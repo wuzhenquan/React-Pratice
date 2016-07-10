20e33f92097769ddd4881a9edb2c7c3f1dd1d093
wuzhenquan <wuzhenquan1990@foxmail.com>
2016-07-11 00:51:47 +0800

    add veryVeryEasyHelloWorld-react-redux

---
+++ my-redux-example/veryVeryEasyHelloWorld-react-redux/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: 'index.jsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel', // 'babel-loader' is also a legal name to reference
+            query: {
+                presets: ['es2015','react']
+            }
+        }]
+    }
+};
