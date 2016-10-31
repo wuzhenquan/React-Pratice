b7a01170879b547ab6b30c1e51de0e61318294ef
巫镇泉 <wuzhenquan1990@foxmail.com>
2016-11-01 00:00:59 +0800

    update

--- login/src/actions/login.js
+++
@@ -1,8 +0,0 @@
-/* Populated by react-webpack-redux:action */
-import {cac} from '../utils'
-
-export const INPUT_USERNAME = 'INPUT_USERNAME'
-export const INPUT_PASSWORD = 'INPUT_PASSWORD'
-
-export const inputUsername = cac(INPUT_USERNAME, 'value')
-export const inputPassword = cac(INPUT_PASSWORD, 'value')
