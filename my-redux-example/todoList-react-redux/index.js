7f5c1c5e82c5464ad9fdc8b091f2f7d398e279de
巫镇泉 <wuzhenquan1990@foxmail.com>
2016-12-03 18:12:21 +0800

    update

--- my-redux-example/todoList-react-redux/index.js
+++ my-redux-example/todoList-react-redux/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
-import todoApp from './reducers'
+import todoApp from './reducers/reducers.js'
 
 let store = createStore(todoApp)
 let rootElement = document.getElementById('root')
--- my-redux-example/todoList-react-redux/reducers/reducers.js
+++ my-redux-example/todoList-react-redux/reducers/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
+import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from '../actions/actions.js'
 const { SHOW_ALL } = VisibilityFilters
 
 function visibilityFilter(state = SHOW_ALL, action) {
